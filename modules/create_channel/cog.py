bdd4ab997a553f8c29ed964bbc8fec171c205883
DenverCoder1 <jonah@freshidea.com>
2021-01-15 02:35:25 +0200

    mark channels as nsfw

--- modules/create_channel/cog.py
+++ modules/create_channel/cog.py
@@ -12,6 +12,7 @@ class CreateChannelCog(commands.Cog):
 
 	def __init__(self, bot):
 		self.bot = bot
+		self.is_nsfw = True
 
 	@commands.command(name="createchannel")
 	@commands.has_role(MYSTERY_HUNT_ROLE_ID)
@@ -27,7 +28,7 @@ class CreateChannelCog(commands.Cog):
 			# reply to user
 			await ctx.send(f"Creating channel {name} in {category}!")
 			# create channel
-			await guild.create_text_channel(name, category=category)
+			await guild.create_text_channel(name, category=category, nsfw=self.is_nsfw)
 		# no argument passed
 		else:
 			# reply to user
